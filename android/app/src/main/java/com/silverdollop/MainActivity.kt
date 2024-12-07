3e0693b310ec5c0a32c864c0a77c7c6710567fed
xebec19 <rohant946@gmail.com>
2024-12-07 22:35:40 +0530

    wip splash screen

--- android/app/src/main/java/com/silverdollop/MainActivity.kt
+++ android/app/src/main/java/com/silverdollop/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "silverdollop"
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null)
   }
 
